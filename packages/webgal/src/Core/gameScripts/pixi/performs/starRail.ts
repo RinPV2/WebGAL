0dcf7178e4c8fd4863220c6bdf6eafc3c2b93b01
RinPV2 <lry19990402@outlook.com>
2024-10-25 03:23:07 +0900

    星轨效果

--- packages/webgal/src/Core/gameScripts/pixi/performs/starRail.ts
+++ packages/webgal/src/Core/gameScripts/pixi/performs/starRail.ts
@@ -1,32 +1,32 @@
 import * as PIXI from 'pixi.js';
 import { registerPerform } from '@/Core/util/pixiPerformManager/pixiPerformManager';
+import { AdvancedBloomFilter } from '@pixi/filter-advanced-bloom';
 import { WebGAL } from '@/Core/WebGAL';
 import de from '@/translations/de';
+import { t } from 'i18next';
+import { rand } from 'pixi-live2d-display-webgal';
 
 const starRail = () => {
   const effectsContainer = WebGAL!.gameplay!.pixiStage!.effectsContainer;
   const app = WebGAL!.gameplay!.pixiStage!.currentApp!;
   const container = new PIXI.Container();
+  const bloomFilter = new AdvancedBloomFilter({
+    threshold: 0.1, // 较低的值会增强光晕效果
+    bloomScale: 2, // 增强光晕强度
+    brightness: 1, // 提高亮度
+    blur: 2, // 增加模糊半径
+    quality: 20, // 增加模糊质量
+  });
+  container.filters = [bloomFilter];
   effectsContainer.addChild(container);
 
   // 将容器移到中心
   container.x = app.screen.width / 2;
-  container.y = app.screen.height / 2;
+  container.y = app.screen.height;
   container.pivot.x = container.width / 2;
   container.pivot.y = container.height / 2;
   container.scale.x = 1;
   container.scale.y = 1;
-
-  const p1 = findTangentPoints(300, -400, 400, 300);
-
-  const graphics = new PIXI.Graphics()
-    .beginFill()
-    .lineStyle(5, 0xffffff, 1)
-    .moveTo(300, -400)
-    .arcTo(p1[0], p1[1], 400, 300, 500)
-    .endFill();
-
-  container.addChild(graphics);
 
   // 随机生成数值函数
   function random(min: number, max: number): number {
@@ -57,7 +57,6 @@ const starRail = () => {
     const length = (r * r) / dm;
     const rx = (mx * length) / dm;
     const ry = (my * length) / dm;
-    console.log(111, mx, my, dm, length, rx, ry);
     return [rx, ry];
   }
 
@@ -79,24 +78,29 @@ const starRail = () => {
     coordinatesCount: number;
     hsl: number;
     width: number;
+    tailAngle: number;
+    tail: number;
+    tailDecay: number;
 
-    constructor(x: number, y: number) {
+    constructor() {
       this.centerX = 0;
-      this.centerY = app.screen.height / 2 - 100;
-      this.x = x;
-      this.y = y;
-      this.radius = Math.sqrt(Math.pow(this.centerX - this.x, 2) + Math.pow(this.centerY - this.y, 2));
+      this.centerY = 0;
+      this.radius = random(app.screen.width / 8, app.screen.width / 1.5);
       this.angle = random(0, Math.PI * 2);
-      this.speed = random(0.003, 0.006);
-      this.hue = random(0, 1) > 0.5 ? random(0, 80) : random(160, 260);
-      this.alpha = 0;
-      this.alphaDecay = random(0.001, 0.015);
-      this.alphaMax = random(0, 1);
+      this.x = this.centerX + Math.cos(this.angle) * this.radius;
+      this.y = this.centerY + Math.sin(this.angle) * this.radius;
+      this.tailAngle = random(Math.PI / 4, Math.PI / 3);
+      this.speed = random(0.005, 0.02) / 5;
+      this.tail = Math.PI / 4;
+      this.tailDecay = this.speed / random(200, 500);
+      this.hue = random(80, 270);
+      this.alphaDecay = random(0.003, 0.006);
+      this.alphaMax = random(0.8, 1);
+      this.alpha = 0.01;
       this.coordinates = [];
-      this.coordinatesCount = random(10, 20);
-      this.coordinates.push([this.x, this.y]);
-      this.hsl = hslToHex(this.hue, 80, 60);
-      this.width = random(0.5, 5);
+      this.coordinatesCount = random(225, 325);
+      this.hsl = hslToHex(this.hue, random(10, 40), random(40, 50));
+      this.width = random(1.5, 3);
 
       this.graphics = new PIXI.Graphics();
       this.graphics.position.set(0, 0);
@@ -111,45 +115,33 @@ const starRail = () => {
 
     // 绘制星星
     draw() {
-      if (
-        (this.coordinates[this.coordinates.length - 1][0] - this.x) *
-          (this.coordinates[this.coordinates.length - 1][0] - this.x) +
-          (this.coordinates[this.coordinates.length - 1][1] - this.y) *
-            (this.coordinates[this.coordinates.length - 1][1] - this.y) >
-        40000
-      )
-        return;
+      const tailX = this.centerX + Math.cos(this.angle + this.tail) * this.radius;
+      const tailY = this.centerY + Math.sin(this.angle + this.tail) * this.radius;
+      const arcStart = findTangentPoints(tailX, tailY, this.x, this.y);
       this.graphics.beginFill();
       this.graphics.clear();
       this.graphics
+        .moveTo(tailX, tailY)
         .lineStyle(this.width, this.hsl, this.alpha)
-        // .moveTo(this.coordinates[this.coordinates.length - 1][0], this.coordinates[this.coordinates.length - 1][1])
-        // .lineTo(this.x, this.y);
-        .arcTo(
-          this.coordinates[this.coordinates.length - 1][0],
-          this.coordinates[this.coordinates.length - 1][1],
-          this.x,
-          this.y,
-          this.radius,
-        );
+        .arcTo(arcStart[0], arcStart[1], this.x, this.y, this.radius);
       this.graphics.endFill();
     }
 
     // 更新星星的位置
     update(delta: number) {
-      while (this.coordinates.length > this.coordinatesCount) {
+      if (this.coordinates.length > this.coordinatesCount) {
         this.coordinates.pop();
       }
       this.coordinates.unshift([this.x, this.y]);
 
-      this.angle += this.speed * delta;
+      this.angle -= this.speed * delta;
       this.x = this.centerX + Math.cos(this.angle) * this.radius;
       this.y = this.centerY + Math.sin(this.angle) * this.radius;
-      if (this.alpha < this.alphaMax) {
-        this.alpha += this.alphaDecay;
+      this.alpha = Math.min(this.alphaDecay + this.alpha, this.alphaMax);
+      if (this.tail < this.tailAngle) {
+        this.tail += this.tailDecay;
       }
-      this.radius += delta;
-      // this.draw();
+      this.draw();
     }
   }
 
@@ -158,20 +150,23 @@ const starRail = () => {
   // 添加多个星星到场景中
   const stars: Star[] = [];
   for (let i = 0; i < starsCount; i++) {
-    const star = new Star(random(0, app.screen.width / 2), random(0, app.screen.height / 2));
+    const star = new Star();
     stars.push(star);
   }
 
   function tickerFn(delta: number) {
     while (stars.length < starsCount) {
-      const star = new Star(random(0, app.screen.width / 2), random(0, app.screen.height / 2));
+      const star = new Star();
       stars.push(star);
     }
     stars.forEach((star) => {
       star.update(delta);
-      if (star.radius > app.screen.width / 1.5) {
-        stars.splice(stars.indexOf(star), 1);
+      if (Math.random() > 0.9995) {
+        star.alphaDecay = -star.alphaDecay * 2;
+      }
+      if (star.alpha <= 0) {
         star.destroy();
+        stars.splice(stars.indexOf(star), 1);
       }
     });
   }
